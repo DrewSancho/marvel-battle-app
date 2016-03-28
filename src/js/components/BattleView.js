9faffad4068a1e1719530f40cd390a6636e76e1a
Drew Sancho <drew_sancho@yahoo.com>
2016-03-28 15:05:01 -0400

    added stats.js

---
+++ src/js/components/BattleView.js
@@ -0,0 +1,9 @@
+var Backbone = require('backbone');
+var _ = require('underscore');
+var $ = require('jquery');
+
+var BattleView = Backbone.View.extend({
+    template: _.template(`
+        
+    `)
+});
