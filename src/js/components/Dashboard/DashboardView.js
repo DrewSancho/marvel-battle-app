698f1784f61726b2baf8815ec552c9f9a717c57b
John <johnwfaulk@gmail.com>
2016-03-31 16:52:55 -0400

    searches collection functional

--- src/js/components/Dashboard/DashboardView.js
+++ src/js/components/Dashboard/DashboardView.js
@@ -1,8 +1,8 @@
 var Backbone = require('backbone');
 var _ = require('underscore');
 var $ = require('jquery');
-var SearchesCollection = require('./SearchesCollection');
-var dispatcher = require('../Events/dispatcher');
+var SearchesView = require('./SearchesView');
+// var dispatcher = require('../Events/dispatcher');
 
 var DashboardView = Backbone.View.extend({
 
@@ -10,24 +10,20 @@ var DashboardView = Backbone.View.extend({
 
     template: _.template(require('./dashboardView.html')),
 
-    initialize: function () {
-        this.collection = SearchesCollection;
-        this.children = [];
+    initialize: function (options) {
+        this.searchesCollection = options.searchesCollection;
+
+        // this.listenTo(this.model, 'sync', function () {
+        //     dispatcher.trigger();
+        // }, this);
+
+        this.searchesView = new SearchesView({ collection: this.searchesCollection });
     },
 
     render: function () {
-        var model, characterView;
-        for (var i = SearchesCollection.length - 1; i >= 0; i--) {
-            model = this.collection.models[i];
-            characterView = new CharacterView({ model: model });
-            this.children.push(characterView);
-            this.$el.append(characterView.$el);
-            characterView.render();
-            // this.$el.append(this.template({
-            //     name: collection.name,
-            //     thumbnail: collection.thumbnail.path + '.' + collection.thumbnail.extension
-            // }));
-        };
+        this.$el.html(this.template());
+        this.searchesView.render();
+        this.$('.searches-slot').append(this.searchesView.$el);
     },
 
     events: {
