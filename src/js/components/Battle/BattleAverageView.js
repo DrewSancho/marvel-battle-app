b88e898cb3f8cb912e62eded8fe9384743e14e8d
Rebekah <rebekah.adair@yahoo.com>
2016-04-01 11:19:12 -0400

    logic for battle average view

--- src/js/components/Battle/BattleAverageView.js
+++ src/js/components/Battle/BattleAverageView.js
@@ -1,8 +1,13 @@
 var Backbone = require('backbone');
 var _ = require('underscore');
-var CharacterModel = require('../Characters/CharacterModel');
+
+var statsCache = require('../Utilities/statsCache.js');
 
 var BattleAverageView = Backbone.View.extend({
+
+    events: {
+        'click .fight': 'fight'
+    },
 
     className: 'battle-average-view',
 
@@ -19,6 +24,16 @@ var BattleAverageView = Backbone.View.extend({
             character1: this.character1.attributes,
             character2: this.character2.attributes
         }));
+    },
+
+    fight: function () {
+        var _this = this;
+        statsCache.get(this.character1.get('id'), function (stats1) {
+            statsCache.get(_this.character2.get('id'), function (stats2) {
+                var x = window.BattleManager.statBattle(stats1, stats2, 10);
+                console.log(x);
+            });
+        });
     }
 });
 
