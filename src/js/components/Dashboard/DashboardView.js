215a246c4344d52ce996c1ec70ce2141edf5dd77
Rebekah <rebekah.adair@yahoo.com>
2016-04-04 09:24:38 -0400

    recent battle collection and views

--- src/js/components/Dashboard/DashboardView.js
+++ src/js/components/Dashboard/DashboardView.js
@@ -3,6 +3,7 @@ var _ = require('underscore');
 var $ = require('jquery');
 var SearchesView = require('./SearchesView');
 var FavoriteCharacterView = require('./FavoriteCharacterView');
+var RecentBattleCollectionView = require('./RecentBattleCollectionView');
 
 var DashboardView = Backbone.View.extend({
 
@@ -17,18 +18,24 @@ var DashboardView = Backbone.View.extend({
     initialize: function (options) {
         this.searchesCollection = options.searchesCollection;
         this.favoriteCharacterCollection = options.favoriteCharacterCollection;
+        this.RecentBattleCollection = options.RecentBattleCollection;
         this.listenTo(this.searchesCollection, 'sync', this.render);
         this.listenTo(this.favoriteCharacterCollection, 'sync', this.render);
+        this.listenTo(this.RecentBattleCollection, 'sync', this.render);
 
         this.favoriteCharacterView = new FavoriteCharacterView({ collection: this.favoriteCharacterCollection });
 
         this.searchesView = new SearchesView({ collection: this.searchesCollection });
+
+        // this.RecentBattleCollectionView = new recentBattleCollectionView({ collection: this.RecentBattleCollection });
     },
 
     render: function () {
         this.$el.html(this.template());
         this.searchesView.render();
         this.favoriteCharacterView.render();
+        // this.recentBattleCollectionView.render();
+        // this.$('.recent-battles-slot').append(this.recentBattleCollectionView.$el);
         this.$('.searches-slot').append(this.searchesView.$el);
         this.$('.random-favorites-slot').append(this.favoriteCharacterView.$el);
     },
